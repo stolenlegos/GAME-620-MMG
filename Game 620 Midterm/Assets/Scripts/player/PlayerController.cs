b0b54b782c481db81a8b227773eb05eae9edd061
CoralineDark <49164730+CoralineDark@users.noreply.github.com>
2021-10-07 20:16:14 -0400

    little mans is animated now

--- Game 620 Midterm/Assets/Scripts/player/PlayerController.cs
+++ Game 620 Midterm/Assets/Scripts/player/PlayerController.cs
@@ -9,15 +9,27 @@ public class PlayerController : MonoBehaviour
 
   [SerializeField]
   private Rigidbody2D rb;
+  private Animator anim; 
+
+    void Start() {
+      anim = GetComponent<Animator>(); 
+    }
 
 
     void Update() {
       movement.x = Input.GetAxisRaw("Horizontal");
       movement.y = Input.GetAxisRaw("Vertical");
+      Animate();
     }
 
 
     void FixedUpdate() {
       rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
+
+    private void Animate(){
+      anim.SetFloat("MovementX", movement.x);
+      anim.SetFloat("MovementY", movement.y);
+    }
 }
+
