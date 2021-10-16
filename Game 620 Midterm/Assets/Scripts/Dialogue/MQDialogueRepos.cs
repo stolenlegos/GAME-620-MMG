3f16ce0e51258620f61e6bd3ebcfdb7a15c2671b
stolenlegos <stolenlegos@gmail.com>
2021-10-15 20:05:23 -0400

    main quest

---
+++ Game 620 Midterm/Assets/Scripts/Dialogue/MQDialogueRepos.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MQDialogueRepos {
+
+    public static string[] MQIntroDialogue = new string[] {
+      "Greetings friend. I hear you want to be an adventurer.",
+      "Haha, that’s good, that’s very good. Adventuring is a great profession, filled with wonderful quests and challenging battles.",
+      "The world could use more and better adventurers and, if you listen to me, you will certainly become one!",
+      "Now, the most important piece of advice I can give you is that adventuring is about balance.",
+      "It is about making sure you are not overwhelmed and are working toward a healthy adventuring life-style.",
+      "So keep that in mind as you start your new life!"
+    };
+
+    public static string[] MQ1Dialogue = new string[] {
+      "Now that you understand what adventuring entails, it’s time for you to start the Adventuring Authorization Process.",
+      "Now I know I mentioned all the “wonderful quests” and “challenging battles,” but before you get to that, you must complete tasks around town to establish yourself.",
+      "The first set of tasks I want you to complete are feats of strength, known colloquially as killing monsters! Sounds fun, right?",
+      "Well don’t get too into the idea of it.",
+      "While it might be the quest that I assigned you, you must also keep up with some of the other tasks around town.",
+      "Remember, balance is key. And don’t worry, everything will be in your quest log. So go forth, and adventure!..."
+    };
+
+    public static string[] MQ1InProgress = new string[] {
+      "I see you haven’t met the requirements yet.",
+      "Remember, check your quest log to see what you need to do. Now, once again, go forth, and adventure!..."
+    };
+
+    public static string[] MQ1Finished = new string[] {
+      "Congratulations! You’ve completed the first stage of the Adventuring Authorization Process!",
+      "I don’t give out gold stars or anything, but I hope my thumbs up is enough!"
+    };
+
+
+
+
+    public static string[] MQ2Dialogue = new string[] {
+      "Now it’s time for stage two. While skill in combat is important for an adventurer, if it is used for evil then, well, I’ll probably get fired.",
+      "Granted no one has the authority to do that since I am the Guildmaster, but whatever.",
+      "Anyway! Your next set of tasks is showing kindness to the townsfolk and helping them out with their lives.",
+      "A lot of people here send out a lot of mail and other items for delivery, so helping them out will be a great kindness toward the town.",
+      "But remember, like last time, it is important to strike a balance.",
+      "Keep your combat skills up and maybe check in around town and see if there are other ways you can help.",
+      "Oh! And everything is still in your quest log, so if you need to know what’s going on, just check there. Now, go forth and adventure!..."
+    };
+
+    public static string[] MQ2InProgress = new string[] {
+      "I see you haven’t met the requirements yet.",
+      "Remember, check your quest log to see what you need to do. Now, once again, go forth, and adventure!..."
+    };
+
+    public static string[] MQ2Finished = new string[] {
+      "Congratulations! You’ve completed the second stage of the Adventuring Authorization Process!",
+      "You’re doing a great job! The town has certainly started to take notice."
+    };
+
+
+
+
+    public static string[] MQ3Dialogue = new string[] {
+      "Now, onto stage 3! While kindness and strength are very important to being a well rounded adventurer, another really important aspect is patience.",
+      "If an adventure can’t take time to understand the task they are doing and complete it thoroughly, they risk rushing into things and getting killed.",
+      "The next set of tasks I’m giving you are going to develop that patience in a simple way: finding lost items!",
+      "Now it seems easy, but searching for things can be hard and certainly takes time, and hard tasks that take time require the most patience of all.",
+      "Don’t put all your time into just these tasks though! People don’t just need adventurers to find their lost belongings, and remember, striking a balance is important.",
+      "Also, just to remind you, everything I’ve told you to do will be in your quest log, so check there if you start wondering what to do next. Now, go forth, and adventure!..."
+    };
+
+    public static string[] MQ3InProgress = new string[] {
+      "I see you haven’t met the requirements yet.",
+      "Remember, check your quest log to see what you need to do. Now, once again, go forth, and adventure!..."
+    };
+
+    public static string[] MQ3Finished = new string[] {
+      "Congratulations! You’ve completed the third stage of the Adventuring Authorization Process!",
+      "You’re over half way to being a full blown adventurer! You should be proud."
+    };
+
+
+
+
+    public static string[] MQ4Dialogue = new string[] {
+      "Now it’s time for the big one! This next stage is a lot, but I think you can handle it.",
+      "What I need you to do is complete a set of tasks like in each of the last stages, but this time you do all the different types of tasks at once.",
+      "Yes, yes, I know that’s a lot, but I believe you can do it.",
+      "And remember, while working to complete this stage, keeping your reputation up is important, so don’t slack on one set of tasks just because you’ve completed what’s required.",
+      "Once again, check your quest log if you are having any trouble. Now go forth, and adventure!..."
+    };
+
+    public static string[] MQ4InProgress = new string[] {
+      "I see you haven’t met the requirements yet.",
+      "Remember, check your quest log to see what you need to do. Now, once again, go forth, and adventure!..."
+    };
+
+    public static string MQ4Finished = "Congratulations, you’ve completed this rigorous stage! I can see you’re tired, but you should be very proud.";
+
+
+
+
+    public static string[] MQ5Dialogue = new string[] {
+      "Now listen closely, because the final stage is the most important.",
+      "When we first met, I told you that the most important part of being an adventurer is balance.",
+      "That balance is not referring to keeping your balance in combat, or balancing which items to deliver, or balancing all the places a person could have lost something.",
+      "No, that balance refers to knowing when you can do a task and when you can’t.",
+      "It means taking breaks and finding time for yourself with an understanding that the things that need to get done will get done.",
+      "It means that, to be the best adventurer for everyone else, you must be the best adventurer for yourself, and the best version of yourself as an adventurer is one where you balance your adventuring and your life.",
+      "A burnt out and ragged adventurer is useless to those who need them.",
+      "Now there is one last stage you must complete before becoming an adventurer.",
+      "I want you to complete one of each of the tasks that you have completed for me before.",
+      "No worries in how long it takes or if people are frustrated with you or not.",
+      " Just complete them on your time, check your quest log when your confused, and when you’re done, return to me. Now go forth, and adventure!"
+    };
+
+    public static string MQ5InProgress = "Take your time. Check your quest log. There’s no rush. Now, once again, go forth, and adventure!...";
+
+    public static string[] MQ5Finished = new string[] {
+      "Congratulations, you are now an official adventurer.",
+      "I am proud of how you have grown through your endeavors, and I hope my wisdom is not lost on you.",
+      "I have been doing this for a long time. I know how hard it can be.",
+      "But just take your time and everything will fall into place.",
+      "Now, for one final time,",
+      "GO FORTH AND ADVENTURE!!!!!"
+    };
+}
