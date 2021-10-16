4ae55bcf215af2446af15af6eb8ae7ccc2103e24
stolenlegos <stolenlegos@gmail.com>
2021-10-15 20:12:57 -0400

    mainquest workings

--- Game 620 Midterm/Assets/Scripts/Questing/Quests/MainQuest.cs
+++ Game 620 Midterm/Assets/Scripts/Questing/Quests/MainQuest.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class MainQuest : MonoBehaviour
 {
+  [SerializeField]
+  GameObject mainQuestUI;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
