446d6a134b23aaef96f181b873a85ee7ca901197
Ilia Kowalzik <31769616+ilk15@users.noreply.github.com>
2021-10-18 06:33:18 -0400

    Add end screen

--- Game 620 Midterm/Assets/Scripts/UI/GoodbyeCard.cs
+++ Game 620 Midterm/Assets/Scripts/UI/GoodbyeCard.cs
@@ -7,17 +7,28 @@ public class GoodbyeCard : MonoBehaviour
 {
     public GameObject qManager;
     public GameObject MQ;
+    public GlobalReputationManager RepManager;
+    public Text Goodbye;
     // Start is called before the first frame update
     void Start()
     {
-       gameObject.SetActive(false); 
+        Goodbye.text = "Don’t be too hard on yourself. \n You've done better than you thought; You were selling yourself short all along.";
+        Goodbye.text +=  "\n Your real reputation was " + RepManager.realReputation + "\n Thank you for playing our game demo!";
+       transform.Find("EndDialogue").gameObject.SetActive(false); 
     }
 
     // Update is called once per frame
     void Update()
-    {
-      if (MQ.GetComponent<MainQuest>().GetStage() == 5 && qManager.GetComponent<MQDManager>().GetLine() == 6) {
-        gameObject.SetActive(true);
+    {   
+        if (qManager.GetComponent<MQDManager>().GetLine() == 5 && MQ.GetComponent<MainQuest>().GetStage() == 6 ) {
+           // Goodbye.text = "Don’t be too hard on yourself. \n You've done better than you thought you did; You've been selling yourself short all along.";
+           // Goodbye.text +=  "Your real reputation was " + RepManager.realReputation + "\n Thank you for playing our game demo!";
+
+
+            if (Input.GetMouseButtonUp(0)){
+                transform.Find("EndDialogue").gameObject.SetActive(true);
+            }
+        
       }
     }
 }
