491824e061b32cd8fb46085ac0022f0b16f196fc
stolenlegos <stolenlegos@gmail.com>
2021-12-06 16:03:01 -0500

    REAL TIMER

--- Final Unity Project/Assets/Scripts/Saturation Objects/Door.cs
+++ Final Unity Project/Assets/Scripts/Saturation Objects/Door.cs
@@ -6,14 +6,12 @@ public class Door : MonoBehaviour {
   private Collider2D col;
   private SpriteRenderer sprite;
   private bool open;
-  [SerializeField] private float setTimer;
-  private float timer;
+
 
   void Start() {
     col = GetComponent<Collider2D>();
     sprite = GetComponent<SpriteRenderer>();
     open = false;
-    timer = setTimer;
     PlayerActions.PushButton += ChangeBool;
   }
 
@@ -26,25 +24,12 @@ public class Door : MonoBehaviour {
       col.enabled = true;
       sprite.enabled = true;
     }
-
-    if (timer <= 0 && open) {
-      ResetTimer();
-      open = false;
-    }
-
-    timer -= Time.deltaTime;
   }
 
 
   private void ChangeBool(GameObject obj) {
     if (obj == this.gameObject){
       open = !open;
-      ResetTimer();
     }
-  }
-
-
-  private void ResetTimer() {
-    timer = setTimer;
   }
 }
