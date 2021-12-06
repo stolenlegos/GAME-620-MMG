c5b238bfacd425440946623de7752b0207dea8f4
solidsmyth <74252115+solidsmyth@users.noreply.github.com>
2021-12-05 22:47:18 -0500

    small movement changes

--- Final Unity Project/Assets/Scripts/Saturation Objects/AllowMovement.cs
+++ Final Unity Project/Assets/Scripts/Saturation Objects/AllowMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class AllowMovement : MonoBehaviour {
   private bool colored;
   private GameObject player;
+  public bool grabbable;
+  private Transform location;
   [SerializeField]
   private Rigidbody2D rb;
 
@@ -17,18 +19,25 @@ public class AllowMovement : MonoBehaviour {
 
 
   void Update() {
+        Debug.Log("Grabbable: " + grabbable);
+
         if (!colored)
         {
+            //Debug.Log("Drop");
             rb.constraints = RigidbodyConstraints2D.FreezeAll;
         }
-        else if (colored && Input.GetMouseButtonDown(0))
+        if (colored && Input.GetMouseButtonDown(0) && grabbable)
         {
+            Debug.Log("Pickup");
             rb.constraints = RigidbodyConstraints2D.None;
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
             this.transform.parent = player.transform;
         }
-        else if (colored && Input.GetMouseButtonUp(0))
+        if ((colored && Input.GetMouseButtonUp(0)) || !colored)
         {
+            Debug.Log("Drop"); 
             this.transform.parent = null;
+
             rb.constraints = RigidbodyConstraints2D.FreezeAll;
         }
 }
@@ -39,4 +48,51 @@ public class AllowMovement : MonoBehaviour {
       colored = !colored;
     }
   }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision != null)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                grabbable = true;
+            }
+            else
+            {
+
+            }
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision != null)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                grabbable = false;
+            }
+            else
+            {
+
+            }
+        }
+    }
+    IEnumerator CheckGrounded()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        while (true)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position - Vector3.up * 1f, -Vector2.up, 0.05f);
+            if (hit.collider != null)
+            {
+                if (hit.transform.tag == "Terrain" || hit.transform.tag == "box_Big" || hit.transform.tag == "box_Small")
+                {
+                    break;
+                }
+            }
+
+            yield return new WaitForSeconds(0.45f);
+        }
+    }
 }
